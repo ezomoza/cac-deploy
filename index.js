7e1bb1f1f85b1186b2523f040f0b4127004f9417
Emanuel Zomoza <114027093+ezomoza@users.noreply.github.com>
2024-06-27 20:39:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Cac deploy')
+  res.send('Cac deploy!!')
 })
 
 const PORT = process.env.PORT || 3000;
